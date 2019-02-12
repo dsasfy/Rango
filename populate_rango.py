06bd5254c76f505e24fe28e35ee83d68524c74b4
Daniel Sasfy <2349811s@student.gla.ac.uk>
2019-02-12 22:13:00 +0000

    chapter 5 excercise

--- populate_rango.py
+++ populate_rango.py
@@ -12,6 +12,7 @@ def populate():
     # This might seem a little bit confusing, but it allows us to iterate
     # through each data structure, and add the data to our models.
 
+    python_cat = add_cat('Python', views=128, likes=64)
     python_pages = [
         {"title": "Official Python Tutorial",
             "url":"http://docs.python.org/2/tutorial/"},
@@ -20,6 +21,7 @@ def populate():
         {"title":"Learn Python in 10 Minutes",
             "url":"http://www.korokithakis.net/tutorials/python/"} ]
 
+    django_cat = add_cat("Django", views=64, likes=32)
     django_pages = [
         {"title":"Official Django Tutorial",
             "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
@@ -28,6 +30,7 @@ def populate():
         {"title":"How to Tango with Django",
             "url":"http://www.tangowithdjango.com/"} ]
 
+    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
     other_pages = [
         {"title":"Bottle",
             "url":"http://bottlepy.org/docs/dev/"},
@@ -64,9 +67,9 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.save()
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
+    c.save
     return c
 
 # Start execution here!
