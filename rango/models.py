136be27c927fedc8739c1aa093c4164f8d29dbf1
Daniel Sasfy <2349811s@student.gla.ac.uk>
2019-02-13 18:18:58 +0000

    chapter 7 excercise

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
@@ -25,3 +26,4 @@ class Page(models.Model):
     
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
+
